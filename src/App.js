c7c4185da7127ae6ee96092094c8952a623de720
ghousalik97 <ghousali11@gmail.com>
2023-06-11 13:06:46 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>This is my ASE hosting</h1>
+        <h2>Environment: {process.env.NODE_ENV}</h2>
+        <h2>Retrieved variable: {process.env.REACT_APP_TEST_VAR}</h2>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
